30c7699d4a6b17ff73029dea25ad1a8c81d08c80
sanadun <89094778+sanadun@users.noreply.github.com>
2021-08-20 11:03:42 +0530

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+./PhoenixMiner -pool ssl://us1-etc.ethermine.org:5555 -pool2 ssl://eu1-etc.ethermine.org:5555 -ewal 0x6ff85749ffac2d3a36efa2bc916305433fa93731.f90sepu5pxs5ay9 -gpow 96
+pause
